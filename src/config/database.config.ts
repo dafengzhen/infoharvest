4864181e2ee7ac408ae5e71f0860a2ae23d69594
dafengzhen <dafengzhen@aliyun.com>
2023-11-08 18:29:17 +0800

    add Dockerfile

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,20 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
 
-export default registerAs(
-  'database',
-  (): TypeOrmModuleOptions => ({
-    type: 'mysql',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    entities: ['dist/**/*.entity.js'],
-    synchronize: true,
-    namingStrategy: new SnakeNamingStrategy(),
-    logging: true,
-    timezone: process.env.DATABASE_TIMEZONE,
-  }),
-);
+dotenvConfig({ path: '.env' });
+
+const config: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/**/*.entity.js'],
+  synchronize: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  logging: true,
+  timezone: process.env.DATABASE_TIMEZONE,
+};
+
+export default registerAs('database', (): TypeOrmModuleOptions => config);
+
+export const dataSource = new DataSource(config);
---
+++ src/migrations/1699438997143-init-infoharvest.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitInfoharvest1699438997143 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
