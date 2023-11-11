bca6bcebf8a41294df07e0b02232f2882592e9b1
dafengzhen <dafengzhen@aliyun.com>
2023-11-11 13:43:25 +0800

    improve search functionality

--- web/app/actions/collections/search-collections-action.tsx
+++ web/app/actions/collections/search-collections-action.tsx
@@ -5,6 +5,8 @@ import FetchDataException from '@/app/exception/fetch-data-exception';
 import { AUTHENTICATION_HEADER } from '@/app/constants';
 import { checkTicket, getQueryParams } from '@/app/common/server';
 import { ICollection } from '@/app/interfaces/collection';
+import SearchExcerptsAction from '@/app/actions/excerpts/search-excerpts-action';
+import FindOneCollectionsAction from '@/app/actions/collections/find-one-collections-action';
 
 export interface ISearchCollectionVariables {
   name: string;
@@ -26,6 +28,16 @@ export default async function SearchCollectionsAction(
   const data = (await response.json()) as ICollection[] | IError;
   if (!response.ok) {
     throw FetchDataException((data as IError).message);
+  }
+
+  const excerpts = await SearchExcerptsAction({ name: variables.name });
+  for (let i = 0; i < excerpts.length; i++) {
+    const collection = await FindOneCollectionsAction({
+      id: excerpts[i].collection!.id,
+    });
+    if (!(data as ICollection[]).find((item) => item.id === collection.id)) {
+      (data as ICollection[]).push(collection);
+    }
   }
 
   return data as ICollection[];
