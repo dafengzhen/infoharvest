1c651496c4c3446c294d8b909cfa1ef046ae6608
dafengzhen <dafengzhen@aliyun.com>
2023-11-11 17:21:31 +0800

    update the matcher configuration of the middleware

--- web/middleware.ts
+++ web/middleware.ts
@@ -11,6 +11,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/collections/:path*',
+    '/excerpts/:path*',
     '/profile/:path*',
     '/settings/:path*',
     '/users/:path*',
