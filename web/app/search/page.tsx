6979ff79c099d6f814fb4f4f032400b2650f58cf
dafengzhen <dafengzhen@aliyun.com>
2024-07-05 23:13:09 +0800

    The subset should return the parent's information

--- web/app/search/page.tsx
+++ web/app/search/page.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next';
 import Search from '@/app/search/search';
+import { Suspense } from 'react';
+import IsLoading from '@/app/components/is-loading';
 
 export const metadata: Metadata = {
   title: 'Search',
 };
 
 export default async function Page() {
-  return <Search />;
+  return (
+    <Suspense fallback={<IsLoading />}>
+      <Search />;
+    </Suspense>
+  );
 }
